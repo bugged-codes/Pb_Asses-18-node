a58c4863a0c12d642490079828e4bd1af466b76c
bugged-codes <vipul_49@outlook.com>
2023-02-20 22:15:04 +0530

    Server Created

---
+++ index.js
@@ -0,0 +1,28 @@
+// Importing express.
+const express = require('express');
+
+// Importing fs and path modules
+const fs = require('fs');
+const path = require('path');
+// Instanciating express as a server.
+const server = express();
+
+// GET method for server using express.
+server.get('/', (req, res) => {
+	res.send('<h1>This is Homepage!</h1>');
+	res.send('<h3>Different pages on server are as follows: </h3><ul><li>/gallary</li><li>/api</li></ul>');
+	res.end();
+});
+
+server.get('/gallary', (req, res) => {
+	res.send('<h1>This is Gallary page</h1>');
+	res.end();
+});
+
+server.get('/api', (req, res) => {
+	res.send('<h2>Get method for api</h2>');
+});
+// POST method for api path
+server.post('/api', (req, res) => {});
+
+server.listen(8000, () => console.log('🥳 Server is running on port 8000'));
